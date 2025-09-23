f013699fa1de02dc8dfacd2002c4d0510cb56cba
ahmedmohmedmesbah-cpu <ahmedmohmedmesbah@gmail.com>
2025-09-23 23:44:52 +0300

    edit 2

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle("active", i === index);
-    });
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+    slide.style.display = i === index ? "block" : "none";
+  });
 }
 
 nextBtn.addEventListener("click", () => {
-    slideIndex = (slideIndex + 1) % slides.length;
-    showSlide(slideIndex);
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
 });
 
 prevBtn.addEventListener("click", () => {
-    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-    showSlide(slideIndex);
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
 });
 
 setInterval(() => {
-    slideIndex = (slideIndex + 1) % slides.length;
-    showSlide(slideIndex);
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
 }, 5000);
 
 showSlide(slideIndex);
